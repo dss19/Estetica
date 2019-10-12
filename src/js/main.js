9f2af6c11ad9c7449a97e728c947711ba16e9cb7
Dmitry <samoylov82@gmail.com>
2019-10-12 17:45:22 +0300

    переделал главную, переделал меню. подготовил блок с преимуществами

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 'use strict'
 $(document).ready(function() {
   // кнопка гамбургер, меню
-  $('.hamburger-btn').on('click', function(e) {
+  $('.header-btn').on('click', function(e) {
     e.preventDefault();
-    $(this).toggleClass('hamburger-btn_active');
+    $(this).toggleClass('header-btn_active');
     $('.menu').toggleClass('menu_active');
   });
 });
