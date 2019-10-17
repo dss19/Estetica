37a16c97a86403ad3baf6f21bfbbcb6bfa9b6cc7
Dmitry <samoylov82@gmail.com>
2019-10-17 12:27:22 +0300

    закончил со слайдерами

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,48 @@ $(document).ready(function() {
     $(this).toggleClass('header-btn_active');
     $('.menu').toggleClass('menu_active');
   });
+
+  // слайдер
+  var mySwiper1 = new Swiper ('.slider1', {
+    // Optional parameters    
+    loop: true,
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+  });
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    // spaceBetween: 2,
+    slidesPerView: 6,
+    // loop: true,
+    freeMode: false,        
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop:true,
+    navigation: {
+      nextEl: '.button-next_light',
+      prevEl: '.button-prev_light',    
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+  
+  // var mySwiper2 = new Swiper ('.slider2', {
+  //   // Optional parameters    
+  //   loop: true,
+  //   navigation: {
+  //     nextEl: '.button-next_light',
+  //     prevEl: '.button-prev_light',
+  //   },
+  //   thumbs: {
+  //     swiper: {
+  //       el: '.swiper-container-thumbs',
+  //       slidesPerView: 6,        
+  //     }
+  //   }
+  // });
 });
